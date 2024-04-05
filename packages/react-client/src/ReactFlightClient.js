e0455fe62a648f541d2e029017465ae4b5f000a8
Sebastian Markbåge <sebastian@calyptus.eu>
2024-04-05 12:48:52 -0400

    Track Owner for Server Components in DEV (#28753)
    This implements the concept of a DEV-only "owner" for Server Components.
    The owner concept isn't really super useful. We barely use it anymore,
    but we do have it as a concept in DevTools in a couple of cases so this
    adds it for parity. However, this is mainly interesting because it could
    be used to wire up future owner-based stacks.
    
    I do this by outlining the DebugInfo for a Server Component
    (ReactComponentInfo). Then I just rely on Flight deduping to refer to
    that. I refer to the same thing by referential equality so that we can
    associate a Server Component parent in DebugInfo with an owner.
    
    If you suspend and replay a Server Component, we have to restore the
    same owner. To do that, I did a little ugly hack and stashed it on the
    thenable state object. Felt unnecessarily complicated to add a stateful
    wrapper for this one dev-only case.
    
    The owner could really be anything since it could be coming from a
    different implementation. Because this is the first time we have an
    owner other than Fiber, I have to fix up a bunch of places that assumes
    Fiber. I mainly did the `typeof owner.tag === 'number'` to assume it's a
    Fiber for now.
    
    This also doesn't actually add it to DevTools / RN Inspector yet. I just
    ignore them there for now.
    
    Because Server Components can be async the owner isn't tracked after an
    await. We need per-component AsyncLocalStorage for that. This can be
    done in a follow up.

--- packages/react-client/src/ReactFlightClient.js
+++ packages/react-client/src/ReactFlightClient.js
@@ -483,6 +483,7 @@ function createElement(
   type: mixed,
   key: mixed,
   props: mixed,
+  owner: null | ReactComponentInfo, // DEV-only
 ): React$Element<any> {
   let element: any;
   if (__DEV__ && enableRefAsProp) {
@@ -492,7 +493,7 @@ function createElement(
       type,
       key,
       props,
-      _owner: null,
+      _owner: owner,
     }: any);
     Object.defineProperty(element, 'ref', {
       enumerable: false,
@@ -509,7 +510,7 @@ function createElement(
       props,
 
       // Record the component responsible for creating this element.
-      _owner: null,
+      _owner: owner,
     }: any);
   }
 
@@ -843,7 +844,12 @@ function parseModelTuple(
   if (tuple[0] === REACT_ELEMENT_TYPE) {
     // TODO: Consider having React just directly accept these arrays as elements.
     // Or even change the ReactElement type to be an array.
-    return createElement(tuple[1], tuple[2], tuple[3]);
+    return createElement(
+      tuple[1],
+      tuple[2],
+      tuple[3],
+      __DEV__ ? (tuple: any)[4] : null,
+    );
   }
   return value;
 }
@@ -1121,12 +1127,14 @@ function resolveConsoleEntry(
     );
   }
 
-  const payload: [string, string, string, mixed] = parseModel(response, value);
+  const payload: [string, string, null | ReactComponentInfo, string, mixed] =
+    parseModel(response, value);
   const methodName = payload[0];
   // TODO: Restore the fake stack before logging.
   // const stackTrace = payload[1];
-  const env = payload[2];
-  const args = payload.slice(3);
+  // const owner = payload[2];
+  const env = payload[3];
+  const args = payload.slice(4);
   printToConsole(methodName, args, env);
 }
 
@@ -1275,7 +1283,10 @@ function processFullRow(
     }
     case 68 /* "D" */: {
       if (__DEV__) {
-        const debugInfo = JSON.parse(row);
+        const debugInfo: ReactComponentInfo | ReactAsyncInfo = parseModel(
+          response,
+          row,
+        );
         resolveDebugInfo(response, id, debugInfo);
         return;
       }
